20df4b189e64e70880d358fb1a867d824c15e0f9
EdsonCasta <dedsoncasta07@gmail.com>
2025-07-08 15:59:05 -0500

    book and author classes

--- catalogodelibros/src/main/java/com/aluracursos/catalogodelibros/model/Libro.java
+++ catalogodelibros/src/main/java/com/aluracursos/catalogodelibros/model/Libro.java
@@ -1,4 +1,43 @@
 package com.aluracursos.catalogodelibros.model;
 
+import java.util.List;
+
 public class Libro {
+
+    private Integer id;
+    private String title;
+    private List<Autor> authors;
+    private List<String> lenguages;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Autor> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<Autor> authors) {
+        this.authors = authors;
+    }
+
+    public List<String> getLenguages() {
+        return lenguages;
+    }
+
+    public void setLenguages(List<String> lenguages) {
+        this.lenguages = lenguages;
+    }
 }
