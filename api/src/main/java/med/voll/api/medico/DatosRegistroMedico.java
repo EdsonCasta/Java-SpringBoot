a9bcd5ccedffaf54a1b0e9fc04c46292a00d9ee7
EdsonCasta <dedsoncasta07@gmail.com>
2025-07-22 18:56:19 -0500

    JPA entities

--- api/src/main/java/med/voll/api/medico/DatosRegistroMedico.java
+++ api/src/main/java/med/voll/api/medico/DatosRegistroMedico.java
@@ -1,12 +1,12 @@
 package med.voll.api.medico;
 
-import med.voll.api.direccion.Direccion;
+import med.voll.api.direccion.DatosDireccion;
 
 public record DatosRegistroMedico(
         String nombre,
         String email,
         String documento,
         Especilidad especialidad,
-        Direccion direccion
+        DatosDireccion datosDireccion
 ) {
 }
