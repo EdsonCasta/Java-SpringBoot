52c575d3ec5e32794cf995c56d67824668f58d9d
EdsonCasta <dedsoncasta07@gmail.com>
2025-07-24 14:18:03 -0500

    Validations with bean validation in patient data

--- api/src/main/java/med/voll/api/paciente/DatosRegistroPaciente.java
+++ api/src/main/java/med/voll/api/paciente/DatosRegistroPaciente.java
@@ -1,12 +1,16 @@
 package med.voll.api.paciente;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.direccion.DatosDireccion;
 
 public record DatosRegistroPaciente(
-        String nombre,
-        String email,
-        String telefono,
-        String documento,
-        DatosDireccion datosDireccion
+        @NotBlank String nombre,
+        @NotBlank String email,
+        @NotBlank String telefono,
+        @NotBlank @Pattern(regexp = "\\d{7,10}") String documento,
+        @NotNull @Valid DatosDireccion direccion
 ) {
 }
