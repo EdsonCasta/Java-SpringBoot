fc0d58eda1b26058bd44725361e0b1942b996e3c
EdsonCasta <dedsoncasta07@gmail.com>
2025-07-28 21:37:48 -0500

    Pagination

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,6 +6,8 @@ import med.voll.api.medico.DatosRegistroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,7 +27,7 @@ public class MedicoController {
    }
 
    @GetMapping
-   public List<DatosListaMedicos> Listar() {
-        return repository.findAll().stream().map(DatosListaMedicos::new).toList();
+   public Page<DatosListaMedicos> Listar(Pageable paginacion) {
+        return repository.findAll(paginacion).map(DatosListaMedicos::new);
    }
 }
